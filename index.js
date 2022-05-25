9e040fc2ed53176002637e46b04070edc8484af5
jyotipadhi1 <jyotipadhi1@gmail.com>
2022-05-25 14:37:31 +0530

    added expressAdvanceQuery function

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-import db from './knex-config.mjs'
+import _ from 'lodash'
+import knex from 'knex'
 
-export { db }
+export const expressAdvanceQuery = async (
+    host,
+    user,
+    password,
+    database,
+    table,
+    query
+) => {
+    let DB = knex({
+        client: 'mysql',
+        connection: {
+            host,
+            user,
+            password,
+            database
+        }
+    })
+
+    let queryObj = DB.from(table)
+    if (query.pagination) {
+        const paginationObj = JSON.parse(query.pagination)
+        const rows = paginationObj.rows || 100
+        queryObj = queryObj
+            .limit(rows)
+            .offset(rows * ((paginationObj.page || 1) - 1))
+    }
+
+    let result = await queryObj
+    if (query.sortField)
+        result = _.orderBy(result, [query.sortField], [query.sort])
+    if (query.where) result = _.filter(result, JSON.parse(query.where))
+    return result
+}
